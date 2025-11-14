9b817962720ee54e594b396c73e561ac6423f7eb
inesprados <inesprazon@gmail.com>
2025-11-14 14:05:35 +0100

    Trigger dni para cliente

---
+++ disparadores.sql
@@ -0,0 +1,24 @@
+
+CREATE OR REPLACE TRIGGER trigger_ControlarSalario
+BEFORE UPDATE OF salario ON EMPLEADOS
+FOR EACH ROW
+BEGIN
+    IF :NEW.salario < OLD.salario THEN
+        RAISE_APPLICATION_ERROR(-20001, 'No se puedo disminuir el salario de un empleado');
+    END IF;
+END;
+/
+
+CREATE OR REPLACE TRIGGER trigger_validarDNI
+BEFORE INSERT OR UPDATE OF DNI ON CLIENTES 
+FOR EACH ROW
+DECLARE
+    cnt_dni number;
+BEGIN
+    IF inserting then
+        select dni into cnt_dni from CLIENTES WHERE dni = :new.dni;
+        IF cnt_dni > 0 THEN
+            RAISE_APPLICATION_ERROR(-2001, 'Ese DNI ya está registrado');
+        END IF;
+END;
+/

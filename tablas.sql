3893b3c0fb9dba3ba559347f32da282029613bae
inesprados <inesprazon@gmail.com>
2025-11-14 12:00:26 +0100

    Tabla vinos

--- tablas.sql
+++ tablas.sql
@@ -13,3 +13,18 @@ CREATE TABLE PRODUCTORES(
     nombre VARCHAR(20) NOT NULL,
     direccion VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE VINOS(
+    codVino VARCHAR(15) PRIMARY KEY,
+    codProductor VARCHAR(15) NOT NULL,
+    marca VARCHAR(20) NOT NULL,
+    anioCosecha DATE NOT NULL,
+    denominacionOrigen VARCHAR(30) NOT NULL,
+    graduacion DOUBLE NOT NULL,
+    viniedoProcedencia VARCHAR(30) NOT NULL,
+    comunidadAutonoma VARCHAR(30) NOT NULL,
+    cantidadProducida INTEGER NOT NULL CHECK (cantidadProducidad>=0),
+    cantidadStock INTEGER NOT NULL CHECK (cantidadStock>=0 AND cantidadStock <= cantidadProducida),
+    CONSTRAINT fk_productor FOREIGN KEY (codProductor)
+        REFERENCES PRODUCTORES(codProductor)
+);

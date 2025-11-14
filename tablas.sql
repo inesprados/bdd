adb29cbeae6a973a466079dc210a314da4795171
inesprados <inesprazon@gmail.com>
2025-11-14 11:44:21 +0100

    Tabla productores

--- tablas.sql
+++ tablas.sql
@@ -1,8 +1,15 @@
 CREATE TABLE CLIENTES(
-    codCliente INTEGER PRIMARY KEY,
-    dni VARCHAR(9),
-    nombre VARCHAR(20),
-    direccion VARCHAR(30),
-    tipoCliente VARCHAR(1) CHECK (tipoCliente IN ('A', 'B', 'C')),
-    comunidadAutonoma VARCHAR(30)
+    codCliente VARCHAR(15) PRIMARY KEY,
+    dni VARCHAR(9) NOT NULL UNIQUE,
+    nombre VARCHAR(20) NOT NULL,
+    direccion VARCHAR(30) NOT NULL,
+    tipoCliente VARCHAR(1) CHECK (tipoCliente IN ('A', 'B', 'C')) NOT NULL,
+    comunidadAutonoma VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE PRODUCTORES(
+    codProductor VARCHAR(15) PRIMARY KEY,
+    dni VARCHAR(9) NOT NULL UNIQUE,
+    nombre VARCHAR(20) NOT NULL,
+    direccion VARCHAR(30) NOT NULL
 );
